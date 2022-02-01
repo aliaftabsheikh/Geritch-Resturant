7295d5e32d1e99cd5fb6f41ede8d693783b60afd
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-01 11:37:54 +0500

    Special Menu
    Complete responsive special menu.

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -11,10 +11,11 @@ const SpecialMenu = () => (
 
     <div className="app__specialMenu-menu">
       <div className="app__specialMenu-menu_wine flex__center">
-        <p className="app__specialMenu_menu_heading">Wine and Beer</p>
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) => (
-            <p>{wine.title}</p>
+            // <p>{wine.title}</p>
+            <MenuItem key ={wine.title + index} title= {wine.title} price ={wine.price} tags= {wine.tags}/>
           ))}
         </div>
       </div>
@@ -24,10 +25,12 @@ const SpecialMenu = () => (
       </div>
 
       <div className="app__specialMenu-menu_cocktails flex__center">
-        <p className="app__specialMenu_menu_heading">Wine and Beer</p>
+        <p className="app__specialMenu_menu_heading">Cocktails</p>
         <div className="app__specialMenu_menu_items">
           {data.cocktails.map((cocktail, index) => (
-            <p>{cocktail.title}</p>
+            // <p>{cocktail.title}</p>
+            <MenuItem key = {cocktail.title + index} title= {cocktail.title} price={cocktail.price}  tags= {cocktail.tags}/>
+
           ))}
         </div>
       </div>
