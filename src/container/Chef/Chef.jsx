51a2e3bcccee6a9373ba8c6587a7b5d4fa865322
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-01 13:05:11 +0500

    Chef
    Complete responsive chef section.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import {images} from '../../constants'
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef image" />
+    </div>
+
+<div className="app__wrapper_info">
+  <SubHeading title="Chef's Word"/>
+  <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+  <div className="app__chef-content">
+    <div className="app__chef-content_quote">
+      <img src={images.quote} alt="quote" />
+      <p className="p__opensans">Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit Auctor Sit .</p>
+    </div>
+      <p className='p__opensans'>Auctor Sit Iaculis In Arcu. Vulputate Nulla Lobortis Mauris Eget Sit. Nulla Scelerisque Scelerisque Congue Ac Consequat, Aliquam Molestie Lectus Eu. Congue Iaculis Integer Curabitur Semper Sit Nunc.</p>
+    <div className="app__chef-sign">
+      <p>Kevin Luo</p>
+      <p className='p__opensans'>Chef & Founder</p>
+      <img src={images.sign} alt="sign" />
+    </div>
+
+  </div>
+</div>
+
   </div>
 );
 
