37165ee307363e4a59064de9387b94d9f110dc84
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-03 16:06:08 +0500

    Gallery
    Gallery Structure.

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -8,8 +8,10 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
     <div className="app__laurels_awards-card">
       <img src={imgUrl} alt="award" />
       <div className="app__laurels_awards-card_content"></div>
-      <p className="p__cormorant" style={{color : '#DCCA87'}}>{title} </p>
-     <p className="p__cormorant" >{subtitle} </p>
+      <p className="p__cormorant" style={{ color: "#DCCA87" }}>
+        {title}
+      </p>
+      <p className="p__cormorant">{subtitle} </p>
     </div>
   );
 };
