bf1cb163afdef8b462e532233bb7011cc09188bd
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-04 13:05:05 +0500

    Footer / NewsLetter
    Complete responsive footer / newsletter.

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import {FiFacebook, FiInstagram, FiTwitter} from 'react-icons/fi'
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay/>
+    <Newsletter/>
   </div>
 );
 
