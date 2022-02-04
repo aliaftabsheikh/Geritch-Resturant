89cdcd4b2b7d595655153e6a39cc648ee2da9109
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-04 14:36:07 +0500

    Awards
    Fix some mistake.

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -7,11 +7,12 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => {
   return (
     <div className="app__laurels_awards-card">
       <img src={imgUrl} alt="award" />
-      <div className="app__laurels_awards-card_content"></div>
+      <div className="app__laurels_awards-card_content">
       <p className="p__cormorant" style={{ color: "#DCCA87" }}>
         {title}
       </p>
       <p className="p__cormorant">{subtitle} </p>
+      </div>
     </div>
   );
 };
